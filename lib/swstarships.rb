31754b8cf7266a2679a9dcd4e0241a5c0ce3c30b
Wesley <wes.beck21@outlook.com>
2020-11-30 11:30:47 -0800

    first commit

---
+++ lib/swstarships.rb
@@ -0,0 +1,6 @@
+require "swstarships/version"
+
+module Swstarships
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/swstarships/version.rb
@@ -0,0 +1,3 @@
+module Swstarships
+  VERSION = "0.1.0"
+end
